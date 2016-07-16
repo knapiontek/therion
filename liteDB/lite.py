43e2aec0321c11ac4bf536535299296355b662bd
knapiontek <knapiontek@nuodb.com>
2016-07-16 22:30:40 +0100

    lite.py changed

--- liteDB/lite.py
+++ liteDB/lite.py
@@ -5,35 +5,37 @@ int: i32
 
 # single line comment
 
-{
+{*
     multi-line comment
-}
+ *}
 
-address-town: (town: string)
-address: address-town (street: string, house-no: int)
+address-town: { town: string }
+address: address-town { street: string, house-no: int }
 address-ref: address
-my-address: address (town: 'my-town', street: 'my-street', house-no: 1)
-your-address: address ('your-town', 'your-street', 2)
+my-address: address { town: 'my-town', street: 'my-street', house-no: 1 }
+your-address: address { 'your-town', 'your-street', 2 }
 their-address: address
-   town: 'my-town'
-   street: 'my-street'
+   town: 'their-town'
+   street: 'their-street'
    house-no: 3
 
 family:
-    last-name: string,
-    first-name-seq: string[],
+    last-name: string
+    first-name-seq: string[]
     address-seq: address[]
 
 family-seq: family[]
 
 family-seq << family
-    'Smith',
-    ['John', 'Mark', 'Teddy', 'Betty'],
-    [(street: 'Avenue'), (street: 'Strand')]
+    'Smith'
+    ['John', 'Mark', 'Teddy', 'Betty']
+    [{ street: 'Avenue' }, { street: 'Strand' }]
 
-item: (street: address.street, house-no: address.house-no, family.last-name)
+simple-function: { x: i32, y: i32 } -> x + y
 
-find-by-name (street: address.street, name: string):
+item: { street: address.street, house-no: address.house-no, family.last-name }
+
+find-by-name: { street: address.street, name: string } ->
     family-seq[last-name = 'Smith' & first-name-seq[name]]
-        .address-seq[address.street = street]
-            item (address.street, house-no, last-name)
+            .address-seq[street = street]
+        item { street, house-no, last-name }
