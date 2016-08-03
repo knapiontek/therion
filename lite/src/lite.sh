a5358d80800c9e918230be43760b6f194bb1cc90
knapiontek <knapiontek@nuodb.com>
2016-08-03 15:56:42 +0100

    improving build

--- lite/src/lite.sh
+++ lite/src/lite.sh
@@ -1,3 +1,7 @@
-rm -rf ../bin && mkdir ../bin
-g++ -c -m64 -pipe -std=c++14 -g -Wall -W -fPIE -I. -I. -o ../bin/test.o test/test.cpp
-g++ -m64 -o ../bin/test ../bin/test.o -lbfd
+srcdir=`dirname $0`
+bindir=/tmp/bin
+printf "src: $srcdir\nbin: $bindir\n"
+rm -rf $bindir && mkdir $bindir
+g++ -c -m64 -pipe -std=c++14 -g -Wall -W -fPIE -I$srcdir -o $bindir/test.o $srcdir/test/test.cpp
+g++ -m64 -o $bindir/test $bindir/test.o -lbfd && $bindir/test
+printf "done\n"
