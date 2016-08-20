c2774dfdf760a32d2e7a610c249b7ec64f8578cb
kris <knapiontek@gmail.com>
2016-08-20 18:31:30 +0100

    update lite.sh

--- lite/src/lite.sh
+++ lite/src/lite.sh
@@ -10,9 +10,9 @@ cd $srcdir
 
 # compile
 lemon lang/grammar.lem $bindir \
-    && clang++-3.6 -c -m64 -pipe -std=c++14 -g -Wall -W -fPIE -I. -I$bindir -o $bindir/test.o test/test.cpp \
-    && clang++-3.6 -m64 -o $bindir/test $bindir/test.o -lbfd \
-    && $bindir/test /home/knapiontek/therion/lite
+    && clang++-3.8 -c -m64 -pipe -std=c++14 -g -Wall -W -fPIE -I. -I$bindir -o $bindir/test.o test/test.cpp \
+    && clang++-3.8 -m64 -o $bindir/test $bindir/test.o -lbfd \
+    && $bindir/test ~/therion/lite
 
 # finish
 printf "done\n"
