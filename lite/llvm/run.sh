d9a7e2ee550337a858a5c273fb7df757142dcec9
kris <knapiontek@gmail.com>
2016-08-25 15:57:31 +0100

    HowToUseJIT building added

--- lite/llvm/run.sh
+++ lite/llvm/run.sh
@@ -1,2 +1,3 @@
 clang-3.6 -g test.c -o test
 clang++-3.6 -std=c++14 -S -emit-llvm test.cpp -o test
+clang++-3.8 -g HowToUseJIT.cpp `llvm-config-3.8 --cxxflags --ldflags --system-libs --libs all` -O0 -lffi  -o HowToUseJIT
