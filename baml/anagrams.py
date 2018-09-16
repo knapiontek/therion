2fea2707b75be1eace850e9620f68ce1149fb2be
kris <knapiontek@gmail.com>
2018-09-16 20:48:40 +0100

    baml test

--- baml/anagrams.py
+++ baml/anagrams.py
@@ -8,14 +8,27 @@
 #   - Thread safe implementation
 
 import unittest
+from collections import defaultdict
+
+def encode(word):
+    return ''.join(sorted(word))
 
 class Anagrams:
 
     def __init__(self):
-        self.words = open('words.txt').readlines()
+        self.data = defaultdict(list)
+        with open('words.txt') as _file:
+            for word in _file.read().splitlines():
+                key = encode(word)
+                self.data[key].append(word)
 
     def get_anagrams(self, word):
-        pass
+        """
+        No threading.Lock() is needed since we have only readers, writer is in __init__ before threads are created.
+        Moreover if we implement insert(word) there is still GIL (global interpreter lock) and build-in classes are thread-safe.
+        """
+        key = encode(word)
+        return self.data[key]
 
 
 class TestAnagrams(unittest.TestCase):
