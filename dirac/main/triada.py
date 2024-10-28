d355634aa1350b45b5ddaa6155217d0cc81d53d3
Kris <knapiontek@gmail.com>
2024-10-28 21:55:54 +0000

    works

--- dirac/main/triada.py
+++ dirac/main/triada.py
@@ -3,23 +3,43 @@ import pyvista as pv
 
 n = 13
 
-nodes = np.zeros((n, n, 3), dtype=np.float64)
+
+def n0(x1: int, y1: int):
+    return x1 * n + y1
+
+
+def n1(x1: int, y1: int):
+    return x1 * (n - 1) + y1
+
+
+nodes = np.zeros((n ** 2, 3), dtype=np.float64)
 
 for x in range(n):
     for y in range(n):
-        nodes[x, y] = [x, y, 0]
+        nodes[n0(x, y)] = [x, y, 0]
 
-edges = np.zeros(((n - 1), (n - 1), 6, 3), dtype=np.float64)
+edges_xy = np.zeros(((n - 1) ** 2, 6), dtype=np.int32)
+edges_x = np.zeros(((n - 1), 2), dtype=np.int32)
+edges_y = np.zeros(((n - 1), 2), dtype=np.int32)
 
-for x in range(n - 1):
-    for y in range(n - 1):
-        edges[x, y] = [nodes[x, y], nodes[x + 1, y], nodes[x + 1, y], nodes[x, y + 1], nodes[x, y + 1], nodes[x, y]]
+for x in range(n):
+    for y in range(n):
+        if x == n - 1 and y == n - 1:
+            pass
+        elif x == n - 1:
+            edges_y[y] = [n0(x, y), n0(x, y + 1)]
+        elif y == n - 1:
+            edges_x[x] = [n0(x, y), n0(x + 1, y)]
+        else:
+            edges_xy[n1(x, y)] = [n0(x, y), n0(x + 1, y), n0(x + 1, y), n0(x, y + 1), n0(x, y + 1), n0(x, y)]
+
+edges = np.append(np.hstack(edges_xy), np.append(np.hstack(edges_x), np.hstack(edges_y)))
 
 if __name__ == '__main__':
     pl = pv.Plotter()
 
-    pl.add_points(np.vstack(nodes), color='darkblue', point_size=4)
-    pl.add_lines(np.vstack(np.hstack(edges)), color='lightgrey', width=1)
+    pl.add_points(nodes, color='darkblue', point_size=4)
+    pl.add_lines(nodes[edges], color='lightgrey', width=1)
 
     pl.add_axes()
     pl.camera_position = 'xy'
