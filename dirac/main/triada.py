ef2e61a15a5967c471ab4e5e2887eb43c0289125
Kris <knapiontek@gmail.com>
2024-11-16 23:18:57 +0000

    grid done but not uniform, isotropic

--- dirac/main/triada.py
+++ dirac/main/triada.py
@@ -1,4 +1,5 @@
 import sys
+from math import sqrt
 
 import numpy as np
 import pyvista as pv
@@ -8,39 +9,45 @@ from main.transform import check_conflicts, prepare_equation, prepare_results
 
 np.set_printoptions(precision=2, suppress=True, threshold=sys.maxsize, linewidth=sys.maxsize)
 
-n = 5
-n_1 = n - 1
+nx = 9
+nx_1 = nx - 1
+ny = 9
+ny_1 = ny - 1
+nz = 2
+nz_1 = nz - 1
+w = .5
+h = sqrt(3) / 2
 
 
 def n0(x: int, y: int, z: int):
-    return (x * n ** 2) + (y * n) + z
+    return (x * ny * nz) + (y * nz) + z
 
 
 def prepare_geometry():
-    _nodes = np.zeros((n ** 3, 3), dtype=np.float64)
+    _nodes = np.zeros((nx * ny * nz, 3), dtype=np.float64)
 
-    for x in range(n):
-        for y in range(n):
-            for z in range(n):
-                _nodes[n0(x, y, z)] = [x, y, z]
+    for x in range(nx):
+        for y in range(ny):
+            for z in range(nz):
+                _nodes[n0(x, y, z)] = [x + w * y + w * z, y * h + w * z, z * h]
 
     _edges = []
 
-    for x in range(n):
-        for y in range(n):
-            for z in range(n):
-                if x < n_1:
+    for x in range(nx):
+        for y in range(ny):
+            for z in range(nz):
+                if x < nx_1:
                     _edges += [n0(x, y, z), n0(x + 1, y, z)]
-                if y < n_1:
+                if y < ny_1:
                     _edges += [n0(x, y, z), n0(x, y + 1, z)]
-                if z < n_1:
+                if z < nz_1:
                     _edges += [n0(x, y, z), n0(x, y, z + 1)]
 
-                if x < n_1 and y < n_1:
+                if x < nx_1 and y < ny_1:
                     _edges += [n0(x + 1, y, z), n0(x, y + 1, z)]
-                if y < n_1 and z < n_1:
+                if y < ny_1 and z < nz_1:
                     _edges += [n0(x, y + 1, z), n0(x, y, z + 1)]
-                if x < n_1 and z < n_1:
+                if x < nx_1 and z < nz_1:
                     _edges += [n0(x, y, z + 1), n0(x + 1, y, z)]
 
     _edges = np.array(_edges)
@@ -52,14 +59,15 @@ def prepare_geometry():
 if __name__ == '__main__':
     fixes = {
         n0(0, 0, 0): [1, 1, 1],
-        n0(0, n_1, 0): [1, 0, 1],
-        n0(n_1, 0, 0): [1, 0, 1],
-        n0(n_1, n_1, 0): [1, 0, 1],
+        n0(0, ny_1, 0): [1, 0, 1],
+        n0(nx_1, 0, 0): [1, 0, 1],
+        n0(nx_1, ny_1, 0): [1, 0, 1],
     }
 
     nodes, edges = prepare_geometry()
-    middle = n // 2
-    forces = {n0(middle, middle, n_1): [0, 0, 3000]}
+    mx = nx // 2
+    my = ny // 2
+    forces = {n0(mx, my, 0): [0, 0, 2000]}
     centers = nodes[sorted(forces.keys())]
     arrows = np.array(list(dict(sorted(forces.items())).values()))
 
@@ -71,7 +79,7 @@ if __name__ == '__main__':
 
     X = spsolve(K, F)
     diff = K.dot(X) - F.toarray().flatten()
-    # print(f'precision: {diff.dot(diff)}')
+    print(f'precision: {diff.dot(diff)}')
 
     results = prepare_results(X, nodes, fixes, forces)
 
