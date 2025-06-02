52bf0624362aa387c3acab550eadf6070e8bb28b
Kris <knapiontek@gmail.com>
2025-06-02 18:36:23 +0100

    rotating point 1

--- wave/main.cpp
+++ wave/main.cpp
@@ -15,18 +15,33 @@ int main(int argc, char *argv[])
     Mp4Creator mp4Creator;
     mp4Creator.init(width, height);
 
-    QPen pen(Qt::white);
-    pen.setWidth(5);
+    QPen pen5(Qt::white);
+    pen5.setWidth(5);
+    QPen pen4(Qt::white);
+    pen4.setWidth(4);
+    QPen pen3(Qt::white);
+    pen3.setWidth(3);
 
     for (int i = 0; i < count; ++i) {
         QImage image(width, height, QImage::Format_RGB888);
         image.fill(QColor::fromHsv(255, 255, 255));
 
         QPainter painter(&image);
-        painter.setPen(pen);
         painter.setFont(QFont("Arial", 20));
         painter.drawText(image.rect(), Qt::AlignTop, QString("Frame %1").arg(i+1));
-        painter.drawPoint(220 * cos((double)i/10) + 512, 170 * sin((double)i/10) + 384);
+
+        double p5 = (double)i/10 + 0.5;
+        painter.setPen(pen5);
+        painter.drawPoint(220 * cos(p5) + 512, 170 * sin(p5) + 384);
+
+        double p3 = (double)i/10 + 0.3;
+        painter.setPen(pen4);
+        painter.drawPoint(220 * cos(p3) + 512, 170 * sin(p3) + 384);
+
+        double p1 = (double)i/10 + 0.1;
+        painter.setPen(pen3);
+        painter.drawPoint(220 * cos(p1) + 512, 170 * sin(p1) + 384);
+
         painter.end();
 
         mp4Creator.addFrame(image);
