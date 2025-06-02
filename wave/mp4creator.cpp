84ec91fa30618332f64ad8be544c18dbba7fbd7a
Kris <knapiontek@gmail.com>
2025-06-02 16:50:48 +0100

    mp4creator

---
+++ wave/mp4creator.cpp
@@ -0,0 +1,110 @@
+#include <stdexcept>
+
+#include "mp4creator.h"
+
+Mp4Creator::Mp4Creator()
+    : formatCtx(nullptr)
+    , codecCtx(nullptr)
+    , codec(nullptr)
+    , stream(nullptr)
+    , swsCtx(nullptr)
+    , frame(nullptr)
+    , packet(nullptr)
+    , pts(0)
+{
+
+}
+
+void Mp4Creator::init(int width, int height)
+{
+    const char *filename = "output.mp4";
+
+    avformat_network_init();
+
+    avformat_alloc_output_context2(&formatCtx, nullptr, "mp4", filename);
+    if (!formatCtx) {
+        throw std::runtime_error("fail to create format context");
+    }
+
+    codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    if (!codec) {
+        throw std::runtime_error("fail to find encoder H.264");
+    }
+
+    stream = avformat_new_stream(formatCtx, nullptr);
+    if (!stream) {
+        throw std::runtime_error("fail to create stream");
+    }
+
+    codecCtx = avcodec_alloc_context3(codec);
+    codecCtx->codec_id = AV_CODEC_ID_H264;
+    codecCtx->width = width;
+    codecCtx->height = height;
+    codecCtx->time_base = {1, 30};
+    codecCtx->gop_size = 10;
+    codecCtx->max_b_frames = 1;
+    codecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+    codecCtx->bit_rate = 400000;
+
+    if (formatCtx->oformat->flags & AVFMT_GLOBALHEADER) {
+        codecCtx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    }
+
+    if (avcodec_open2(codecCtx, codec, nullptr) < 0) {
+        throw std::runtime_error("fail to open codec");
+    }
+
+    avcodec_parameters_from_context(stream->codecpar, codecCtx);
+
+    if (!(formatCtx->oformat->flags & AVFMT_NOFILE)) {
+        if (avio_open(&formatCtx->pb, filename, AVIO_FLAG_WRITE) < 0) {
+            throw std::runtime_error("fail to open output file");
+        }
+    }
+
+    int ret = avformat_write_header(formatCtx, nullptr);
+    if (ret < 0) {
+        throw std::runtime_error("fail to write header");
+    }
+
+    swsCtx = sws_getContext(width,
+                            height,
+                            AV_PIX_FMT_RGB24,
+                            width,
+                            height,
+                            AV_PIX_FMT_YUV420P,
+                            SWS_BILINEAR,
+                            nullptr,
+                            nullptr,
+                            nullptr);
+
+    frame = av_frame_alloc();
+    frame->format = AV_PIX_FMT_YUV420P;
+    frame->width = width;
+    frame->height = height;
+
+    av_frame_get_buffer(frame, 32);
+
+    packet = av_packet_alloc();
+}
+
+void Mp4Creator::destroy()
+{
+    // flush
+    avcodec_send_frame(codecCtx, nullptr);
+    while (avcodec_receive_packet(codecCtx, packet) == 0) {
+        packet->stream_index = stream->index;
+        av_interleaved_write_frame(formatCtx, packet);
+        av_packet_unref(packet);
+    }
+
+    av_write_trailer(formatCtx);
+
+    av_packet_free(&packet);
+    av_frame_free(&frame);
+    sws_freeContext(swsCtx);
+    avcodec_free_context(&codecCtx);
+    if (!(formatCtx->oformat->flags & AVFMT_NOFILE))
+        avio_closep(&formatCtx->pb);
+    avformat_free_context(formatCtx);
+}
---
+++ wave/mp4creator.h
@@ -0,0 +1,30 @@
+#ifndef MP4CREATOR_H
+#define MP4CREATOR_H
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavutil/imgutils.h>
+#include <libswscale/swscale.h>
+}
+
+class Mp4Creator
+{
+public:
+    Mp4Creator();
+    void init(int width, int height);
+private:
+    void init();
+    void destroy();
+private:
+    AVFormatContext *formatCtx;
+    AVCodecContext *codecCtx;
+    const AVCodec *codec;
+    AVStream *stream;
+    SwsContext *swsCtx;
+    AVFrame *frame;
+    AVPacket *packet;
+    int64_t pts;
+};
+
+#endif // MP4CREATOR_H
