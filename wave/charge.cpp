0011390df843792b4ea4b675fcd418dbd71c0512
Kris <knapiontek@gmail.com>
2025-06-13 15:56:25 +0100

    painter

--- wave/charge.cpp
+++ wave/charge.cpp
@@ -3,6 +3,7 @@
 #include <QPen>
 #include <QPolygon>
 #include "common.h"
+#include "painter.h"
 #include "solve.h"
 
 const qreal unit = 1;
@@ -79,31 +80,6 @@ void applyForce(InputMesh &inputMesh, qint32 point, qint32 length, qint32 sizeH,
         inputMesh.forceMap.insert(v.i, v.p);
     }
 }
-
-struct Painter : public QPainter
-{
-    void drawArrow(const QPointF &begin, const QPointF &end)
-    {
-        qreal x = end.x() - begin.x();
-        qreal y = end.y() - begin.y();
-
-        qreal length = std::sqrt(x * x + y * y);
-
-        qreal unitX = x / length;
-        qreal unitY = y / length;
-
-        qreal midX = end.x() - 8 * unitX;
-        qreal midY = end.y() - 8 * unitY;
-
-        QPointF p1(midX + 2 * unitY, midY - 2 * unitX);
-        QPointF p2(midX - 2 * unitY, midY + 2 * unitX);
-
-        drawLine(begin, end);
-        QPolygonF polygon;
-        polygon << end << p1 << p2;
-        drawPolygon(polygon);
-    }
-};
 
 void charge(qint32 width, qint32 height, qint32 count, ImageCapture imageCapture)
 {
---
+++ wave/painter.h
@@ -0,0 +1,29 @@
+#ifndef PAINTER_H
+#define PAINTER_H
+
+struct Painter : public QPainter
+{
+    void drawArrow(const QPointF &begin, const QPointF &end)
+    {
+        qreal x = end.x() - begin.x();
+        qreal y = end.y() - begin.y();
+
+        qreal length = std::sqrt(x * x + y * y);
+
+        qreal unitX = x / length;
+        qreal unitY = y / length;
+
+        qreal midX = end.x() - 8 * unitX;
+        qreal midY = end.y() - 8 * unitY;
+
+        QPointF p1(midX + 2 * unitY, midY - 2 * unitX);
+        QPointF p2(midX - 2 * unitY, midY + 2 * unitX);
+
+        drawLine(begin, end);
+        QPolygonF polygon;
+        polygon << end << p1 << p2;
+        drawPolygon(polygon);
+    }
+};
+
+#endif // PAINTER_H
